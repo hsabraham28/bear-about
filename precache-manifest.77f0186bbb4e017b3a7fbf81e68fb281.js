42cbbaece27a7c635721962f2a2eef73acdfab79
Hannah Abraham <hsabraham28@berkeley.edu>
2020-07-17 03:55:08 -0500

    Updates

---
+++ precache-manifest.77f0186bbb4e017b3a7fbf81e68fb281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55bd1ce44880ce3dba8f7e91fd0639d",
+    "url": "/bear-about/index.html"
+  },
+  {
+    "revision": "1dac1435675c8e490204",
+    "url": "/bear-about/static/css/main.3cb29b1d.chunk.css"
+  },
+  {
+    "revision": "92f12719d9cd90d861a0",
+    "url": "/bear-about/static/js/2.b8bfa9ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bear-about/static/js/2.b8bfa9ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dac1435675c8e490204",
+    "url": "/bear-about/static/js/main.c39a08e1.chunk.js"
+  },
+  {
+    "revision": "5b88d37188796aa85c51",
+    "url": "/bear-about/static/js/runtime-main.f35188a3.js"
+  },
+  {
+    "revision": "4b45812deefde2aca043e668fe33c8db",
+    "url": "/bear-about/static/media/oski.4b45812d.png"
+  }
+]);
